efcff532460ae1c46055c17090e947a0fe4ab2b2
denisklopotan <denisklopotan@gmail.com>
2022-08-06 21:22:12 +0000

    Fix allowed hosts

--- green_kitchen/settings.py
+++ green_kitchen/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['green_kitchen.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['green-kitchen.herokuapp.com', 'localhost']
 
 
 # Application definition

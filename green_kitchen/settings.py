91e3b059fd5f3fb986ee2503eb0d446938d1869d
denisklopotan <denisklopotan@gmail.com>
2022-11-11 18:20:29 +0000

    Download django contactus and  install app

--- green_kitchen/settings.py
+++ green_kitchen/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 x_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_summernote',
     'blog',
+    'contactus',
 ]
 
 SITE_ID = 1
--- green_kitchen/urls.py
+++ green_kitchen/urls.py
@@ -18,8 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('', include('blog.urls'), name='blog-urls'),
     path('summernote/', include('django_summernote.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
+    path('', include('contactus.urls')),
 ]
 
